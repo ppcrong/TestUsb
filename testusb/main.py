125b85eb5210ab860009ff61a1e3d50f87eba578
ppcrong <ppcrong@gmail.com>
2020-04-27 15:24:39 +0800

    [ppcrong.20200427][TestUsb] Convert dev generator to list and add find specific device button

--- testusb/main.py
+++ testusb/main.py
@@ -1,7 +1,8 @@
 import sys
-import usb.core
 
-from PyQt5 import QtWidgets, QtGui, QtCore
+import usb
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 
 from ui.main_window import Ui_MainWindow
 
@@ -11,15 +12,53 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.setText('List USB')
-        self.ui.pushButton.clicked.connect(self.list_usb)
+        self.ui.btn_list_usb.setText('List USB')
+        self.ui.btn_list_usb.clicked.connect(self.list_usb)
+        self.ui.btn_find_usb.setText('Find USB')
+        self.ui.btn_find_usb.clicked.connect(self.find_specific_device)
 
     def list_usb(self):
-        dev = usb.core.find(find_all=True)
-        for cfg in dev:
-            print('VendorID={}(0x{:04X}) & ProductID={}(0x{:04X})'.format(str(cfg.idVendor), cfg.idVendor,
-                                                                  str(cfg.idProduct), cfg.idProduct))
-            print('-------------------------------------------------------------------------------------')
+        try:
+            dev = usb.core.find(find_all=True)
+            list_usb = list(dev)
+            for cfg in list_usb:
+                cfg.set_configuration()
+                print('VendorID={}(0x{:04X}) & ProductID={}(0x{:04X})'.format(str(cfg.idVendor), cfg.idVendor,
+                                                                              str(cfg.idProduct), cfg.idProduct))
+                print('-------------------------------------------------------------------------------------')
+        except Exception as e:
+            print(e)
+
+    def find_specific_device(self):
+        # find our device
+        dev = usb.core.find(idVendor=0x3231, idProduct=0x0100)
+
+        # was it found?
+        if dev is None:
+            # raise ValueError('Device not found')
+            QMessageBox.information(self, "Warning", "Device not found", QMessageBox.Close)
+
+        # set the active configuration. With no arguments, the first
+        # configuration will be the active one
+        dev.set_configuration()
+
+        # get an endpoint instance
+        cfg = dev.get_active_configuration()
+        intf = cfg[(0, 0)]
+
+        ep = usb.util.find_descriptor(
+            intf,
+            # match the first OUT endpoint
+            custom_match= \
+                lambda e: \
+                    usb.util.endpoint_direction(e.bEndpointAddress) == \
+                    usb.util.ENDPOINT_OUT)
+
+        assert ep is not None
+
+        # write the data
+        ep.write('test')
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
